fa9c44dc4d1e2cc17d4cdabf083254b7fd6d6778
Ali Stoica Kovacs <alistoicakovacs@icloud.com>
2022-07-21 22:28:18 +0200

    added Live preview to cards

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
 const contactMeButton = document.querySelector(".button");
+const infoButton = document.querySelector(".button-info");
+const infoText = document.querySelector(".about-me-text");
 
 contactMeButton.addEventListener("click", function () {
 	window.open("mailto:alistoicakovacs@icloud.com");
@@ -9,5 +11,4 @@ contactMeButton.addEventListener("click", function () {
 function toggleButton() {
 	navList.classList.toggle("show");
 }
-
 hamburgerButton.addEventListener("click", toggleButton);
