69ff732521fca33c2e66e8957964cc75f17d297a
Ali Stoica Kovacs <alistoicakovacs@icloud.com>
2022-07-21 22:05:46 +0200

    changed contact footer button functionality

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
+const contactMeButton = document.querySelector(".button");
+
+contactMeButton.addEventListener("click", function () {
+	window.open("mailto:alistoicakovacs@icloud.com");
+});
 
 function toggleButton() {
-  navList.classList.toggle("show");
+	navList.classList.toggle("show");
 }
 
 hamburgerButton.addEventListener("click", toggleButton);
